07c4474f2c267e08da50c9a7c98ce12479a0d1ce
Nick Falbo <68254882+nfalbo213@users.noreply.github.com>
2022-11-30 22:48:19 -0500

    switched url back to app

--- backend/stockCheck.js
+++ backend/stockCheck.js
@@ -18,8 +18,8 @@ const optionArr = document.querySelectorAll('.item-input');
 const itmbttnArr = document.querySelectorAll('.add-item-button')
 
 //const url = "./backend/warehouse.json";
-const baseUrl = 'http://localhost:3000';
-//const baseUrl = 'https://target-species-acquired.herokuapp.com';
+//const baseUrl = 'http://localhost:3000';
+const baseUrl = 'https://target-species-acquired.herokuapp.com';
 const path = '/check-stock';
 const url = `${baseUrl}${path}`;
 
